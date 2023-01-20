36e32b4557b5bdc3a3193d6777f23f69e975b9ee
Nicolas <chaplainnicolas10@gmail.com>
2023-01-20 20:07:25 +0100

    :memo: description on format for BERT inputs

---
+++ Bert.py
@@ -0,0 +1,3 @@
+#Pour utiliser un réseau BERT il faut d'abord tokenize une phrase.
+#Pour cela, il faut tokenize la phrase en plusieurs token créer un token de départ, de fin,
+# de padding (pour arriver à la len max). Puis convertir le tout en ID.
