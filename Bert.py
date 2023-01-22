fb9f22255aa2e1df77bafd2241e47bef0b54ce9d
Nicolas <chaplainnicolas10@gmail.com>
2023-01-22 17:28:24 +0100

    :sparkles: Added data processing funtions

--- Bert.py
+++ Bert.py
@@ -1,3 +1,12 @@
 #Pour utiliser un réseau BERT il faut d'abord tokenize une phrase.
 #Pour cela, il faut tokenize la phrase en plusieurs token créer un token de départ, de fin,
 # de padding (pour arriver à la len max). Puis convertir le tout en ID.
+import transformers as trans
+
+tokenizer = trans.BertTokenizer.from_pretrained('bert-base-uncased')
+
+sentence = "Je suis alle en Jamaïque hier soir pour faire du snowboard"
+
+print(sentence)
+print(tokenizer.encode(sentence, add_special_tokens= False))
+print(tokenizer.encode(sentence, add_special_tokens= True))
