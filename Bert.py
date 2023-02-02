4da3a08eaaca899631ddb736173b8f1ed7eb7896
Nicolas <chaplainnicolas10@gmail.com>
2023-02-02 20:03:35 +0100

    :construction: Working on the fine tuning

--- Bert.py
+++ Bert.py
@@ -2,11 +2,25 @@
 #Pour cela, il faut tokenize la phrase en plusieurs token créer un token de départ, de fin,
 # de padding (pour arriver à la len max). Puis convertir le tout en ID.
 from dataset import Dataset
-
+from transformers import BertForSequenceClassification, Trainer
 DS = Dataset
 DS.SetFilename(DS, filename='Projet-3A\Data\data.csv')
 DS.SetModel(DS, 'bert-base-uncased')
 max_length = DS.max_length(DS)
-#On tokenize les données car le BERT n'accepte que ce format en tant qu'input et/ou pour être entraine.
-sentence = "Je suis alle en Jamaïque hier soir pour faire du snowboard"
-print(DS.Encode(DS,sentence, True, max_length))
+
+#On tokenize les données.En effet, le BERT n'accepte que ce format en tant qu'input et/ou pour être entraine.
+
+#Import du modèle BERT :
+BERTmodel = BertForSequenceClassification.from_pretrained(
+    "bert-base-uncased", # Use the 12-layer BERT model, with an uncased vocab.
+    num_labels = 2, # The number of output labels--2 for binary classification.
+                    # You can increase this for multi-class tasks.   
+    output_attentions = False, # Whether the model returns attentions weights.
+    output_hidden_states = False, # Whether the model returns all hidden-states.
+)
+#Configuration pour le fine-tuning:
+#AJOUTER TOUT LES VALIDATION TEST ETC. VOIR CETTE VIDEO:
+#https://huggingface.co/docs/transformers/training onglet 'TRAIN'.
+
+
+#Entrainement du modele BERT utilisé:
